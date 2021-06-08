d0c7ea46ccc5784b0120784f6356e5496c74c238
Alex Dobrin <alex.dan.dobrin@gmail.com>
2021-06-08 09:22:06 +0300

    Add login page

---
+++ Plantech-iOS/Controllers/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Plantech-iOS
+//
+//  Created by Alex Dobrin on 08/06/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "loginSuccess", sender: self)
+    }
+    
+    @IBAction func registerClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "loginToRegister", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Plantech-iOS/Controllers/RegisterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterViewController.swift
+//  Plantech-iOS
+//
+//  Created by Alex Dobrin on 08/06/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registerClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "registerSuccess", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
